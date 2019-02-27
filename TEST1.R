4a19b1706c7fa9c2f843065751d7bfe0234ddf32
Milou Sep <msc.sep@gmail.com>
2019-02-27 15:08:39 +0100

    test2

--- TEST1.R
+++ TEST1.R
@@ -1,1 +1,10 @@
 # TEST1
+
+# READ file from OSF
+# https://rdrr.io/github/SachaEpskamp/OSF2R/man/readOSF.html
+
+#install.packages("devtools")
+library(devtools)
+install_github("SachaEpskamp/OSF2R")
+
+# Sys.setlocale('LC_ALL','C')
