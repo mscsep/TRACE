3e9fa4b1c713a92447c34f962cc888171eabe0a3
Milou Sep <msc.sep@gmail.com>
2019-02-27 16:46:42 +0100

    load data from OSF

--- Load_data.R
+++ Load_data.R
@@ -1,31 +1,28 @@
 # Load data from OSF
 
 # READ file from OSF
-# https://rdrr.io/github/SachaEpskamp/OSF2R/man/readOSF.html
-
-
-library(rJava)
-#install.packages("devtools")
-library(devtools)
-#install_github("SachaEpskamp/OSF2R") # error heeft iets met rJava te maken.. WERKT NIET
-
-
 # https://centerforopenscience.github.io/osfr/
-# install.packages("remotes")
-remotes::install_github("centerforopenscience/osfr") # werkt wel
+# OSFR package https://www.rdocumentation.org/packages/osfr/versions/0.1.0.9000
 
+# install.packages("remotes")
+# remotes::install_github("centerforopenscience/osfr") # werkt wel
 library(osfr)
 
 # authentication OSF private repositories.
 # http://centerforopenscience.github.io/osfr/articles/auth.html
-
-# osf_auth() # Paste in console met PAT
-
+# Paste in console met PAT (handmatig toevoegen)
+ osf_auth()
 
 # Retrieve OSF file(s)
 TRACE<- osf_retrieve_file("9uryb")
+# Open dataset with browser
+osf_open(TRACE) 
 
-library 
+# Download local copy of OSF data.
+osf_download(TRACE) 
+
+# Load file in envirionment.
+Trace_OSF_Test <- read_delim("Trace_OSF_Test.csv", ";", escape_double = FALSE, trim_ws = TRUE)
 
 
 
