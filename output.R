79ad7509729140a2f4ca6b1cb611c6a5792eb092
Milou Sep <msc.sep@gmail.com>
2019-03-07 13:05:12 +0100

    contrast coding checked & new comparison added, looked at bonferroni corrections

--- output.R
+++ output.R
@@ -15,6 +15,7 @@ TRACE_output <- function (model, title, subtitle){
   
   ## ---> CODE needs to be checked!
   # some info: Test linear combinations (http://www.metafor-project.org/doku.php/tips:testing_factors_lincoms)
+  # http://www.metafor-project.org/doku.php/tips:multiple_factors_interactions
   
   # RQ2 "Main effects of
   Neutral <- anova(model, L = c(.5,.5, 0, 0))
@@ -23,6 +24,10 @@ TRACE_output <- function (model, title, subtitle){
   ## posthoc RQ2. is learning or memory most effected by valence? (Posthoc)
   phase.neutral <- anova(model, L = c(-1,1, 0, 0)) 
   phase.stressful <- anova(model, L = c(0,0, -1, 1)) # learning -
+  
+  # Posthoc RQ2 influence of valence on learning or memory
+  valence.learning <-anova(model, L = c(-1,0, 1, 0)) # neutral = -
+  valence.memory <- anova(model, L = c(0,-1, 0, 1))
   
   
   #RQ3: neutral and stressful learning and memory in  ## PRECIES hetzelfde als de values in ht model!
@@ -34,7 +39,7 @@ TRACE_output <- function (model, title, subtitle){
   
   ##Summary results organized in table
   #resultMain <- data.frame(matrix(data = NA, nrow = 17, ncol = 8))
-  resultMain <- data.frame(matrix(data = NA, nrow = 10, ncol = 8))
+  resultMain <- data.frame(matrix(data = NA, nrow = 12, ncol = 8))
   
   
   colnames(resultMain) <- c("test", "ci.lb", "ci.ub", 
@@ -43,43 +48,61 @@ TRACE_output <- function (model, title, subtitle){
   
   resultMain[,1] <- c("Learning", "Memory", "Neutral", "Stressful",
                       "phase.neutral", "phase.stressful",
+                      "valence.learning", "valence.memory",
                       "nLearning", "nMemory", "sLearning", "sMemory")
   
   resultMain[,4] <- round(c(Learning$Lb, Memory$Lb, 
                             Neutral$Lb, Stressful$Lb,
                             phase.neutral$Lb, phase.stressful$Lb,
+                            valence.learning$Lb, valence.memory$Lb,
                             model$beta), digits = 4) #effect size
   
   resultMain[,5] <- round(c(Learning$se, Memory$se, 
                             Neutral$se, Stressful$se,
                             phase.neutral$se, phase.stressful$se,
+                            valence.learning$se, valence.memory$se,
                             model$se), digits = 4) #se
   
   resultMain[,6] <- round(c(Learning$zval, Memory$zval, 
                             Neutral$zval, Stressful$zval,
                             phase.neutral$zval, phase.stressful$zval,
+                            valence.learning$zval, valence.memory$zval,
                             model$zval), digits = 4)  #zvalues
   
   resultMain[,7] <- round(c(Learning$pval, Memory$pval, 
                             Neutral$pval, Stressful$pval,
                             phase.neutral$pval, phase.stressful$pval,
+                            valence.learning$pval, valence.memory$pval,
                             model$pval), digits = 4)  #pvalues
   
   resultMain[,2] <- round(resultMain[,4] - (resultMain[,5] * 1.96), digits = 4) #CI lower 
   resultMain[,3] <- round(resultMain[,4] + (resultMain[,5] * 1.96), digits = 4) #CI upper
   
   
-  # Beter checken!
-  resultMain[,8] <- round(c(p.adjust(resultMain[ 1,7], method = "bonferroni", n = 2), # 2 groeps compared in learning
+  # Beter checken! (not needed for now? only report model effect ("different from 0"), not yet the difference between each other.))
+  resultMain[,8] <- round(c(p.adjust(resultMain[ 1,7], method = "bonferroni", n = 2), # learning (I think 2, as each estimate is used once for a phase contras (L or M) and once for a valencce contrast (S,N))
                             p.adjust(resultMain[ 2,7], method = "bonferroni", n = 2), # memory
                             p.adjust(resultMain[ 3,7], method = "bonferroni", n = 2), # neutral
                             p.adjust(resultMain[ 4,7], method = "bonferroni", n = 2), # stressful
-                            p.adjust(resultMain[ 5,7], method = "bonferroni", n = 1), # l vs m (neutral) --> correction needed?
-                            p.adjust(resultMain[ 6,7], method = "bonferroni", n = 1), # l vs m (stress)
-                            resultMain[7,7],
-                            resultMain[8,7],
+                            p.adjust(resultMain[ 5,7], method = "bonferroni", n = 2), # l vs m (neutral) (also 2 for all posthocs?)
+                            p.adjust(resultMain[ 6,7], method = "bonferroni", n = 2), # l vs m (stress)
+                            p.adjust(resultMain[ 7,7], method = "bonferroni", n = 2), # neutral vs stres (learning)
+                            p.adjust(resultMain[ 8,7], method = "bonferroni", n = 2), # neutral vs stres (memory)
+                            
+                            
                             resultMain[9,7],
-                            resultMain[10,7]), digits = 4) #pvalue bonf correction
+                            resultMain[10,7],
+                            resultMain[11,7],
+                            resultMain[12,7]), digits = 4) #pvalue bonf correction
+  
+  
+  ## posthoc RQ2. is learning or memory most effected by valence? (Posthoc)
+  phase.neutral <- anova(model, L = c(-1,1, 0, 0)) 
+  phase.stressful <- anova(model, L = c(0,0, -1, 1)) # learning -
+  
+  # Posthoc RQ2
+  valence.learning <-anova(model, L = c(-1,0, 1, 0)) # neutral = -
+  valence.memory <- anova(model, L = c(0,-1, 0, 1))
   
   
   
@@ -110,7 +133,7 @@ TRACE_output <- function (model, title, subtitle){
     ggplot(plot_data, 
                aes(x = test, y = effectsize, fill=valence)) +
      
-    ylab("Standardized mean difference (CI)") +
+    ylab("Hedge's G (CI)") + # could also be "Standardized mean difference"
      ggtitle(title, subtitle = subtitle) +
 
       facet_wrap(.~ phase, scales='free_x', strip.position = "bottom" )+
