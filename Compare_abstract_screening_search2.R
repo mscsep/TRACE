2d4740bbad6a722cbbce93359b02673b92a2d30f
mscsep <msc.sep@gmail.com>
2020-05-12 12:20:21 +0200

    code updated to merge second screening & earlier data

--- Compare_abstract_screening_search2.R
+++ Compare_abstract_screening_search2.R
@@ -58,8 +58,6 @@ human %>% mutate(
       inclusion_MS==1 ~"yes",
       inclusion_MS==0 ~"no",
       grepl("?",inclusion_MS, fixed = T) ~ '?')
-  # inclusion_SH=
-  #   tidyr::replace_na(.$inclusion_SH, "?") # is this correct?
 )->human_recoded
 
 animal %>% mutate(
@@ -68,15 +66,13 @@ animal %>% mutate(
       inclusion_MS==1 ~"yes",
       inclusion_MS==0 ~"no",
       grepl("?",inclusion_MS, fixed = T) ~ '?')
-  # Inclusion_SH=
-  #   tidyr::replace_na(.$Inclusion_SH, "?") # is this correct?
 ) ->animal_recoded
 
 
 # select equal rows
 # df.a2 %>% filter((Reference_PMID_SH==Reference_PMID_MS) & (Inclusion_SH == inclusion_MS)) %>% nrow()
-df.a2 %>% mutate(
-  conclusion= case_when(
+animal_recoded %>% mutate(
+  conclusion1= case_when(
     # Inclusion_SH == '?' ~ 'full_text_check',
     # Inclusion_MS == '?' ~ 'full_text_check',
     (Reference_PMID_SH!=Reference_PMID_MS) ~'PMIDincor',
@@ -84,29 +80,29 @@ df.a2 %>% mutate(
     (Reference_PMID_SH==Reference_PMID_MS) & (Inclusion_SH == inclusion_MS) ~ 'same',
     (Reference_PMID_SH==Reference_PMID_MS) & (Inclusion_SH != inclusion_MS) ~ 'discuss'
   )
-)->df.a2 
+)->animal_recoded2 
 
-df.h2 %>% mutate(
-  conclusion= case_when(
+human_recoded %>% mutate(
+  conclusion1= case_when(
     (Reference_PMID_SH!=Reference_PMID_MS) ~'PMIDincor',
     (Reference_PMID_SH==Reference_PMID_MS) &  (inclusion_SH == inclusion_MS) ~ 'same',
     # (Reference_PMID_SH==Reference_PMID_MS) & (inclusion_SH != '?' | inclusion_MS!="?") & (inclusion_SH == inclusion_MS) ~ 'same',
     # (Reference_PMID_SH==Reference_PMID_MS) & (inclusion_SH == '?' | inclusion_MS=="?") ~ 'full_text_check',
     (Reference_PMID_SH==Reference_PMID_MS) & (inclusion_SH != inclusion_MS) ~ 'discuss'
  )
-) ->df.h2 
+) ->human_recoded2 
 
 # select papers to discuss
 # df.h2 %>% filter(conclusion=='discuss') %>% write.csv2(.,file='inconsistencies_human.csv')
 # df.a2 %>% filter(conclusion=='discuss') %>% write.csv2(.,file='inconsistencies_animal.csv')
 
 # create vectors with PMIDs that need to be discussed:
-df.h2$Reference_PMID_MS[which(df.h2$conclusion=='discuss')]->pmid.h
-df.a2$Reference_PMID_MS[which(df.a2$conclusion=='discuss')]->pmid.a
+human_recoded2$Reference_PMID_MS[which(human_recoded2$conclusion=='discuss')]->pmid.h
+animal_recoded2$Reference_PMID_MS[which(animal_recoded2$conclusion=='discuss')]->pmid.a
 
 # select original entries for these PMIDs
-human %>% filter(Reference_PMID_MS %in% pmid.h) %>% write.csv2(.,file='inconsistencies_human_s2.csv')
-animal %>% filter(Reference_PMID_MS %in% pmid.a) %>% write.csv2(.,file='inconsistencies_animal_s2.csv')
+human_recoded2 %>% filter(Reference_PMID_MS %in% pmid.h) %>% write.csv2(.,file='inconsistencies_human_s2.csv')
+animal_recoded2 %>% filter(Reference_PMID_MS %in% pmid.a) %>% write.csv2(.,file='inconsistencies_animal_s2.csv')
 
 # upload files to OSF
   osf_upload(osf_retrieve_node("awkn6"),'inconsistencies_human_s2.csv')
@@ -115,12 +111,75 @@ animal %>% filter(Reference_PMID_MS %in% pmid.a) %>% write.csv2(.,file='inconsis
 #remove downloaded files
 file.remove(c("TRACE_screening_search2_SH.xlsx","TRACE_screening_search2_MS.xlsx","inconsistencies_human_s2.csv", "inconsistencies_animal_s2.csv"))
 
-##### Merged discussed inconsitencies
+
+# Merge data after discussion inconsitencies ------------------------------
+# 12.5.20
+
 osf_retrieve_file("ugyrp") %>% osf_download() # discussed animal
 osf_retrieve_file("kve78") %>% osf_download() # discussed human data
 
-
 animal_discussed <- read.csv2("inconsistencies_animal_s2_SH.csv")
 human_discussed <- read.csv2("inconsistencies_human_s2_SH.csv")
+#remove colums with only na's
+human_discussed <- human_discussed[,colSums(is.na(human_discussed))<nrow(human_discussed)]
+
+# human_discussed <- human_discussed[rowSums(is.na(human_discussed))<nrow(human_discussed),]
+# animal_discussed <- animal_discussed[rowSums(is.na(animal_discussed))<nrow(animal_discussed),]
 
 
+# merge colums from discussion to original screening data
+animal_discussed %>% select(Reference_PMID_SH,!grep("_SH|_MS", colnames(animal_discussed)))->animal_discussed2
+human_discussed %>% select(Reference_PMID_SH,!grep("_SH|_MS", colnames(human_discussed)))->human_discussed2
+
+# merge data
+full_join(human_recoded2,human_discussed2, by="Reference_PMID_SH")->human_new
+full_join(animal_recoded2,animal_discussed2, by="Reference_PMID_SH")->animal_new
+
+
+# Create colum with new descisions:
+
+# rename "Second.screening..yes...inclusie..no..exlusie....discussieren" to more convenient colname
+ colnames(human_new)[40]<-"second.screening"
+ colnames(animal_new)[43]<-"second.screening"
+ 
+# new conclusion colum human
+human_new %>% mutate(
+  conclusion2=
+    case_when(
+      conclusion1 == "same"  ~ inclusion_MS,
+      (conclusion1 == "discuss" & (second.screening =="yes"| second.screening =="no")) ~ as.character(second.screening),
+      (conclusion1 == "discuss" & (second.screening !="yes"| second.screening !="no")) ~ as.character(Final),
+     # (conclusion1 == "discuss" & !is.na(second.screening) & is.na(Final) ) ~ as.character(second.screening),
+     # (conclusion1 == "discuss" & !is.na(second.screening) )  ~ as.character(Final))
+      )
+)->human_new2
+# check, there should be no na's
+is.na(human_new2$conclusion2) %>% any()
+
+# new conclusion colum human
+animal_new %>% mutate(
+  conclusion2=
+    case_when(
+      conclusion1 == "same"  ~ inclusion_MS,
+      (conclusion1 == "discuss" & (second.screening =="yes"| second.screening =="no")) ~ as.character(second.screening),
+      (conclusion1 == "discuss" & (second.screening !="yes"| second.screening !="no")) ~ as.character(Final),
+      # (conclusion1 == "discuss" & !is.na(second.screening) & is.na(Final) ) ~ as.character(second.screening),
+      # (conclusion1 == "discuss" & !is.na(second.screening) )  ~ as.character(Final))
+    )
+)->animal_new2
+# check, there should be no na's
+is.na(animal_new2$conclusion2) %>% any()
+
+# export new datasets
+human_new2 %>% filter(conclusion2 == "?") %>% write.csv2(.,file='required_full_checks_human_s2.csv')
+animal_new2 %>% filter(conclusion2 == "?") %>% write.csv2(.,file='required_full_checks_animal_s2.csv')
+
+# upload files to OSF
+ osf_upload(osf_retrieve_node("awkn6"),'required_full_checks_human_s2.csv')
+ osf_upload(osf_retrieve_node("awkn6"),'required_full_checks_animal_s2.csv')
+
+#remove downloaded files
+file.remove(c("inconsistencies_human_s2_SH.csv", "inconsistencies_animal_s2_SH.csv"))
+# file.remove(c('required_full_checks_human_s2.csv', 'required_full_checks_animal_s2.csv'))
+ 
+ 
